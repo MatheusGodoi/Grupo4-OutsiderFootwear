0789fb3d864292e9d42ab1bbd10a859c54a10362
Henrique Hiram Libutti Nunez <henrique.nunez@yahoo.com>
2021-11-18 15:54:53 -0300

    Feat: login and sign up pages

--- Milestone_2/outsider-footwear/src/pages/SignIn/index.tsx
+++ Milestone_2/outsider-footwear/src/pages/SignIn/index.tsx
@@ -1,9 +1,48 @@
-import { Container } from "./styles"
+import { Link } from 'react-router-dom';
+import { Container, Button, UserInput, UserFieldsContainer, PasswordInput } from "./styles"
+
+import signInImg from '../../assets/undraw_enter_uhqk_1.svg'
 
 export default function SignIn() {
-    return (
-        <Container>
-            Você está no Sign In
-        </Container>
-    )
+  return (
+    <Container>
+      
+      <img src={signInImg} alt="Outsider Footwear" />
+      
+      <UserFieldsContainer>
+        <p style={{gridArea: "sign_in_label", fontSize: "28px"}}>
+          Sign In
+        </p>
+
+        <p style={{gridArea: "email_label", alignSelf: "center"
+}}>
+          Email 
+        </p>
+        <UserInput />
+
+        <p style={{gridArea: "password_label", alignSelf: "center"}}>
+          Password 
+        </p>
+        <PasswordInput />
+        
+        <p style={{gridArea: "login_button", alignSelf: "center"}}>
+          <Link to='/'>
+            <button>
+              Login
+            </button>
+          </Link>
+        </p>
+
+        <p style={{gridArea: "forgot_password_link", alignSelf: "center", justifySelf: "left"}}>
+          <Link to='/'>
+            Forgot Password?
+          </Link>
+        </p>
+
+        <p style={{gridArea: "sign_up_link", alignSelf: "center", justifySelf: "center"}}>
+          New here? <Link to='/signup'>Sign up</Link>
+        </p>
+      </UserFieldsContainer>
+    </Container>
+  )
 }
